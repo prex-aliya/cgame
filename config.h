9a2453241cd9199e547bc9dcbb8ca442550a541b
prex-aliya <you@example.com>
2023-08-02 11:07:29 -1000

    add header file for real

---
+++ config.h
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+
+const bool debug=true;
+unsigned int level=1;
+unsigned short int frames=1;
+
+unsigned int height=7;
+unsigned int width=15;
+unsigned short int mapx=20;
+unsigned short int mapy=10;
+
+unsigned int playerx=8;
+unsigned int playery=4;
+bool playermove=true;
+bool playerview=true;
