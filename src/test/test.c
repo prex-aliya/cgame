74d5e9896ac95791862a80c4100dffd6d571b5ef
prex-aliya <droved-through@proton.me>
2023-08-12 16:48:06 -1000

    update

--- src/test/test.c
+++ src/test/test.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
-int main() {
+#define BBLACK   "\x1b[40m"
+#define BWHITE   "\x1b[47m"
+#define RESET    "\x1b[0m"
 
-  int x1 = 3;
-  int y1 = 2;
-  int x2 = 15;
-  int y2 = 5;
-
-  int m_new = 2 * (y2-y1);
-  int slope_error_new = m_new - (x2 - x1);
-  for (int nx = x1, ny = y1; nx <= x2; nx++ ) {
-    printf("%d, %d\n", nx, ny);
-
-    slope_error_new += m_new;
-
-    if (slope_error_new >= 0) {
-      ny++;
-      slope_error_new -= 2*(x2-x1);
+unsigned int_to_binary(unsigned k) {
+  printf("%d", k);
+    if (k == 0) {
+      return 0;
     }
-  }
+    if (k == 1) {
+      return 1;                       /* optional */
+      }
+    return (k % 2) + 10 * int_to_binary(k / 2);
+}
 
+int main() {
+  printf("%d", int_to_binary(12));
 }
